4b67478af59f30bc61aa101ad88f19fe1eeeb349
austinTracy7 <101482688+austinTracy7@users.noreply.github.com>
2022-07-16 22:48:03 -0600

    Create __init__.py

---
+++ get_most_uncommon_words/__init__.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from nltk.corpus import brown
+
+if __name__ == "__main__":
+    annotated_tokens_df = pd.read_pickle("tokens_and_meanings.pkl")
+
+    # pull in the brown corpus
+
+    # rank the words according to their frequency in that corpus
+
+    # select the ten least frequent words to start with
+
+    
